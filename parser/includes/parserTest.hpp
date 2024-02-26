e5b313b566ce85f7af148069ffefa336084a5e9e
rdragan <rdragan@student.42heilbronn.de>
2024-02-26 10:30:52 +0100

    compare vectors

--- parser/includes/parserTest.hpp
+++ parser/includes/parserTest.hpp
@@ -1,5 +1,29 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
+#include <string>
 
 void	runTests();
+
+// Compartions utils
+template <typename T>
+void	compareVectors(const std::vector<T>& out, const std::vector<T>& expected)
+{
+	if (out.size() != expected.size())
+	{
+		std::cout << "Vector size [out " << out.size() << "]" << " [expected " << expected.size() << "]" << std::endl;
+	}
+	else
+	{
+		typename std::vector<T>::const_iterator	i = out.begin();
+		typename std::vector<T>::const_iterator	end = out.end();
+		int							index = 0;
+		for (; i < end; ++i)
+		{
+			if (out[index] != expected[index]) std::cout << "Vector out != expected" << std::endl;
+			return ;
+			index++;
+		}
+	}
+}
