fd7d8f9eb3b5e27bb5032b33992274972e9dab01
rdragan <rdragan@student.42heilbronn.de>
2024-02-27 14:54:10 +0100

    limit password to 50 char

--- parser/srcs/commands/Pass.cpp
+++ parser/srcs/commands/Pass.cpp
@@ -2,7 +2,7 @@
 
 Pass::Pass(const std::string& text)
 {
-	if (text.length() > 1024) throw std::invalid_argument("Too long string");
+	if (text.length() > 54) throw std::invalid_argument("Too long string");
 	std::vector<std::string>	tmp = split(text);
 	if (tmp.size() != 2) throw std::invalid_argument("Invalid syntax");
 	_password = tmp[1];
