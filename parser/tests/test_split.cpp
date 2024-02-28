95cc4ce6bbff1bad222949df665b4e87e6727b7b
rdragan <rdragan@student.42heilbronn.de>
2024-02-28 18:42:27 +0100

    pass all tests for test_split

--- parser/tests/test_split.cpp
+++ parser/tests/test_split.cpp
@@ -8,7 +8,30 @@ void	testEmptySplit()
 	ASSERT_EQUAL(actual.size(), expected.size(), "testEmptySplit() => The sizes of the vectors are different!");
 }
 
+void	testOneItemSplit()
+{
+	std::vector<std::string>	actual = split("Hola");
+	std::vector<std::string>	expected;
+
+	expected.push_back("Hola");
+	ASSERT_EQUAL(actual.size(), expected.size(), "testOneItemSplit() => The sizes of the vectors are different!");
+	ASSERT_EQUAL(actual[0], expected[0], "testOneItemSplit() => The element 0 is different!");
+}
+
+void	testTwoItemSplit()
+{
+	std::vector<std::string>	actual = split("Hola mundo");
+	std::vector<std::string>	expected;
+
+	expected.push_back("Hola");
+	expected.push_back("mundo");
+	ASSERT_EQUAL(actual.size(), expected.size(), "testTwoItemSplit() => The sizes of the vectors are different!");
+	ASSERT_EQUAL(actual[0], expected[0], "testTwoItemSplit() => The element 0 is different!");
+	ASSERT_EQUAL(actual[1], expected[1], "testTwoItemSplit() => The element 0 is different!");
+}
+
 void	test_split()
 {
 	testEmptySplit();
+	testOneItemSplit();
 }
