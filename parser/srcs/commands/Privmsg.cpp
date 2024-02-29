07234286fca95e6cf26712513eb2f1ff410982ec
rdragan <rdragan@student.42heilbronn.de>
2024-02-29 11:13:19 +0100

    improve documentation

--- parser/srcs/commands/Privmsg.cpp
+++ parser/srcs/commands/Privmsg.cpp
@@ -13,15 +13,13 @@ Privmsg::Privmsg(const std::vector<std::string>& cmd) : _target(""), _hasUser(fa
 	if (cmd.size() <= 3 && cmd[2] == ":") throw std::invalid_argument("Invalid syntax");
 	if (cmd[2][0] == ':')
 	{
+		// join in _message all from cmd[2](excluding ':') till cmd[n]
 		_message += cmd[2].substr(1);
 		for (size_t i = 3; i < cmd.size(); i++) _message += " " + cmd[i];
 	}
-	// join in _message all from cmd[2] till cmd[n]
 }
 
-Privmsg::~Privmsg()
-{
-}
+Privmsg::~Privmsg() {}
 
 const std::string&	Privmsg::getTarget() const { return _target; }
 const std::string&	Privmsg::getMessage() const { return _message; }
