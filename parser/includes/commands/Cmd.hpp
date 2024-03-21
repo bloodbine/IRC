fee7a0b402935f177318972e59688bc745036784
rdragan <rdragan@student.42heilbronn.de>
2024-03-21 10:30:42 +0100

    remove pure virtual functions

--- parser/includes/commands/Cmd.hpp
+++ parser/includes/commands/Cmd.hpp
@@ -8,7 +8,4 @@ class Cmd
 public:
 	Cmd() {}
 	virtual ~Cmd() {}
-	virtual const std::string&	getTarget() const = 0;
-	virtual const std::string&	getMessage() const = 0;
-	virtual bool				getHasUser() const = 0;
 };
