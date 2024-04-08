c1da2f0e579f0b2aa0bfdaf35c746787ed9e5a9b
rdragan <rdragan@student.42heilbronn.de>
2024-04-08 15:58:46 +0200

    add comment with needed numeric replies

--- srcs/commands/Nick.cpp
+++ srcs/commands/Nick.cpp
@@ -3,16 +3,15 @@
 
 char* Nick::ERR_NONICKNAMEGIVEN(const std::string& serverIp) const
 {
-	// Need to have access to the server ip
 	std::string tmp = serverIp;
 	tmp += "\t431 ::No nickname given\n";
 	char	*out = new char[tmp.size() + 1];
 	std::strcpy(out, tmp.c_str());
 	return (out);
 }
-
-// Handle ERR_NONICKNAMEGIVEN
-
+//            ERR_ERRONEUSNICKNAME
+//            ERR_NICKNAMEINUSE               ERR_NICKCOLLISION
+//            ERR_UNAVAILRESOURCE             ERR_RESTRICTED
 
 Nick::Nick(const std::vector<std::string>& vec) : _size(vec.size()) {}
 
