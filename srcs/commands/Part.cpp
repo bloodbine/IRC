0eb7c6c9cb984765f3d21df3eb491b189ac16651
madou9 <issoufou.mamadou@yahoo.com>
2024-04-15 19:57:23 +0200

    some improve

--- srcs/commands/Part.cpp
+++ srcs/commands/Part.cpp
@@ -5,14 +5,14 @@ Part::Part(Client* client, const std::vector<std::string>& vec): _client(client)
 {
     if (!_client->GetIsRegistered()) ERR_NOTREGISTERED();
     if (_size < 1) ERR_NEEDMOREPARAMS("USER");
-    
     _channelName = vec[1]; // extract channel name in index 1
-    //check if the channel is exist
+
 }
 
 char* Part::execute() const
 {
     std::cout << ":" << _client->GetIdenClient() << " PART " << _channelName << std::endl;
+    //check if the channel is exist
      if (!server::channelExists(_channelName)) ERR_NOSUCHCHANNEL();
     // check if channel as user
     Channel* channel = server::getChannelByName(_channelName);
