ecbc6c921844e240d2aa59a2759de9aa2a239431
rdragan <rdragan@student.42heilbronn.de>
2024-04-25 11:42:14 +0200

    add reasson to Part

--- srcs/commands/Part.cpp
+++ srcs/commands/Part.cpp
@@ -1,11 +1,16 @@
 #include "commands/Part.hpp"
 #include "server.hpp"
 
-Part::Part(Client* client, const std::vector<std::string>& vec): _client(client), _size(vec.size())
+Part::Part(Client* client, const std::vector<std::string>& vec): _client(client), _size(vec.size()), _reasson("")
 {
     if (!_client->GetIsRegistered()) ERR_NOTREGISTERED();
     if (_size < 1) ERR_NEEDMOREPARAMS("USER");
     _channelName = vec[1]; // extract channel name in index 1
+    if (_size >= 3)
+    {
+        _reasson += vec[2];
+        for (size_t i = 3 ; i < _size; i++) _reasson += " " + vec[i];
+    }
 
 }
 
@@ -17,6 +22,7 @@ char* Part::execute() const
     // check if channel as user
     Channel* channel = server::getChannelByName(_channelName);
     if (!channel->hasUser(*_client)) ERR_NOTONCHANNEL();
+    std::cout << "reasson: " << _reasson << std::endl;
     channel->removeUser(*_client);
     return strdup("Bye Bye \n");
 }
