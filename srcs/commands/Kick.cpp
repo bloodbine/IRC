0e63d968ca4e26bb5e3cda187a07b2942225dae2
rdragan <rdragan@student.42heilbronn.de>
2024-04-27 12:05:01 +0200

    comment notregistered for testing purposes

--- srcs/commands/Kick.cpp
+++ srcs/commands/Kick.cpp
@@ -3,7 +3,7 @@
 
 Kick::Kick(Client* client, const std::vector<std::string>& vec): _client(client), _size(vec.size()), _channel(""), _nick(""), _reasson("")
 {
-    if (!_client->GetIsRegistered()) ERR_NOTREGISTERED();
+    // if (!_client->GetIsRegistered()) ERR_NOTREGISTERED();
     if (_size < 3) ERR_NEEDMOREPARAMS("KICK");
     // More than 3 char throw invalid SYNTAX ERROR.
 
@@ -18,7 +18,8 @@ Kick::Kick(Client* client, const std::vector<std::string>& vec): _client(client)
 	// Read reasson
 	if (_size >= 4)
 	{
-		_reasson += vec[3].substr(1);
+		if (vec[3][0] == ':') _reasson = vec[3].substr(1);
+		else _reasson = vec[3];
 		for (size_t i = 4; i < _size; i++) _reasson += " " + vec[i];
 	}
 }
