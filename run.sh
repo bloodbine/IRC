361e5eebbe664962d0afc9fcfa7fe3089ea5e9e4
rdragan <rdragan@student.42heilbronn.de>
2024-04-29 14:39:03 +0200

    add script to server in a container

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $# != 1 ]; then
+	echo "[+] ./run.sh <port>"
+	exit 1
+fi
+
+if [ $(docker images | head -2 | tail -1 | awk '{print $1}') != "irc" ]; then
+	docker build -t irc .
+fi
+
+docker run --rm -p$1:$1 -itv $PWD:/irc irc
