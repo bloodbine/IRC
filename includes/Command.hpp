d79eddb9cbaa105b2f985e5a696632cddcea5e7a
madou9 <issoufou.mamadou@yahoo.com>
2024-05-04 14:35:13 +0200

    create function handlePing

--- includes/Command.hpp
+++ includes/Command.hpp
@@ -19,12 +19,14 @@ private:
 	std::string	_realName;
 	std::string	_channelName;
 	std::string	_chanKey;
+	std::string	_serverName;
 
 
 	void	handlePass();
 	void	handleNick();
 	void	handleUser();
 	void	handleJoin();
+	void	handlePing();
 public:
 	Command(const std::vector<std::string>& vec, Client *client);
 	void	printStringToSend() const;
