d853dfbb9db0d772cc1e00bc9d5e8495f0af1033
rdragan <rdragan@student.42heilbronn.de>
2024-05-09 12:40:31 +0200

    more debugging info

--- run_irc_serv.sh
+++ run_irc_serv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-make && valgrind --leak-check=full ./ircserv $PORT a
+make && valgrind --leak-check=full --show-leak-kinds=all ./ircserv $PORT a
 if [ $? != 0 ]; then
-	make re && valgrind --leak-check=full ./ircserv $PORT a
+	make re && valgrind --leak-check=full --show-leak-kinds=all ./ircserv $PORT a
 fi
